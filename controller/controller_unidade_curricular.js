56a71d9371ca04ff409432fcb0112c0c8d8fa9a6
Yasmin Gonçalves <yasmin.coelho4@senaisp.edu.br>
2023-06-02 13:52:51 -0300

    DAO e Controller da Unidade Curricular concluídos

--- controller/controller_unidade_curricular.js
+++ controller/controller_unidade_curricular.js
@@ -10,16 +10,17 @@ var message = require('./modulo/config.js')
 
 var unidade_curricularDAO = require('../model/DAO/unidade_curricularDAO.js')
 
+//Insere uma nova unidade curricular
 const inserirUnidadeCurricular = async function (dadosUnidadeCurricular) {
 
     if (dadosUnidadeCurricular.nome == '' || dadosUnidadeCurricular.nome == undefined || dadosUnidadeCurricular.nome.length > 200 ||
-        dadosUnidadeCurricular.descricao == '' || dadosUnidadeCurricular.descricao == undefined || dadosUnidadeCurricular.descricao.length > 200
+        dadosUnidadeCurricular.descricao.length > 200
     ) {
         return message.ERROR_REQUIRED_FIELDS //400
 
     } else {
         let resultDadosUnidadeCurricular = await unidade_curricularDAO.insertUnidadeCurricular(dadosUnidadeCurricular)
-        
+
         //Tratamento para ver se o banco inseriu corretamente os dados
         if (resultDadosUnidadeCurricular) {
             let dadosUnidadeCurricularJSON = {}
@@ -38,8 +39,117 @@ const inserirUnidadeCurricular = async function (dadosUnidadeCurricular) {
     }
 }
 
+//Atualiza uma unidade curricular
+const atualizarUnidadeCurricular = async function (dadosUnidadeCurricular, idUnidadeCurricular) {
+    if (dadosUnidadeCurricular.nome == '' || dadosUnidadeCurricular.nome == undefined || dadosUnidadeCurricular.nome.length > 200 ||
+        dadosUnidadeCurricular.descricao.length > 200
+    ) {
+        return message.ERROR_REQUIRED_FIELDS // 400
 
+    } else if (idUnidadeCurricular == '' || idUnidadeCurricular == undefined || isNaN(idUnidadeCurricular)) {
+        return message.ERROR_INVALID_ID //400
+
+    } else {
+        dadosUnidadeCurricular.id = idUnidadeCurricular
+        let dadosUnidadeCurricularJSON = {}
+
+        let statusId = await unidade_curricularDAO.selectByIdUnidadeCurricular(idUnidadeCurricular)
+
+        if (statusId) {
+            let resultDadosUnidadeCurricular = await unidade_curricularDAO.updateUnidadeCurricular(dadosUnidadeCurricular)
+
+            let unidadeCurricularId = await unidade_curricularDAO.selectByIdUnidadeCurricular(idUnidadeCurricular)
+
+            if (resultDadosUnidadeCurricular) {
+                dadosUnidadeCurricularJSON.message = message.SUCCESS_UPDATED_ITEM.message
+                dadosUnidadeCurricularJSON.status = message.SUCCESS_UPDATED_ITEM.status //200
+                dadosUnidadeCurricularJSON.unidade_curricular = unidadeCurricularId
+
+                return dadosUnidadeCurricularJSON
+            } else {
+                return message.ERROR_INTERNAL_SERVER //500
+            }
+
+        } else {
+            return message.ERROR_NOT_FOUND //404
+        }
+    }
+}
+
+//Retorna a unidade curricular filtrando pelo ID
+const getBuscarUnidadeCurricularID = async function (id) {
+
+    //Verifica se o usuário digitou corretamente
+    if (id == '' || isNaN(id) || id == undefined) {
+        return message.ERROR_INVALID_ID
+    } else {
+        let dadosUnidadeCurricularJSON = {}
+        let dadosUnidadeCurricular = await unidade_curricular.selectByIdUnidadeCurricular(id)
+
+        if (dadosUnidadeCurricular) {
+            //Criando um JSON com o atributo unidade curricular, para encaminhar um array de unidades curriculares
+            dadosUnidadeCurricularJSON.status = message.SUCCESS_REQUEST.status
+            dadosUnidadeCurricularJSON.message = message.SUCCESS_REQUEST.messages
+            dadosUnidadeCurricularJSON.unidade_curricular = dadosUnidadeCurricular
+
+            return dadosUnidadeCurricularJSON
+            
+        } else {
+            return message.ERROR_NOT_FOUND
+        }
+    }
+}
+
+//Retorna a lista de todos as unidades curriculares
+const getUnidadesCurriculares = async function () {
+    let dadosUnidadesCurricularesJSON = {}
+
+    //Chama a função do arquivo DAO que irá retornar todos os registros do BD
+    let dadosUnidadeCurricular = await unidade_curricularDAO.selectAllUnidadesCurriculares()
+
+    if (dadosUnidadeCurricular) {
+        dadosUnidadesCurricularesJSON.status = message.SUCCESS_REQUEST.status
+        dadosUnidadesCurricularesJSON.message = message.SUCCESS_REQUEST.message
+        dadosUnidadesCurricularesJSON.quantidade = dadosUnidadeCurricular.length
+        dadosUnidadesCurricularesJSON.unidades_curriculares = dadosUnidadeCurricular
+
+        return dadosUnidadesCurricularesJSON
+
+    } else {
+        return message.ERROR_NOT_FOUND //404
+    }
+
+}
+
+
+const deletarUnidadeCurricular = async function (id) {
+    if (id == ' ' || id == undefined || isNaN(id) || id == null) {
+        return message.ERROR_INVALID_ID //400
+
+    } else {
+        let buscarById = await getBuscarUnidadeCurricularID(id)
+
+        //Verifica se a unidade curricular existe, se não existir, envia o retorno da função (getBuscarUnidadeCurricularID)
+        if (buscarById.status == 404) {
+            return buscarById
+
+        //Se a unidade curricular existir, prossegue e deleta
+        } else {
+            let resultDadosUnidadeCurricular = await unidade_curricularDAO.deleteUnidadeCurricular(id)
+
+            if (resultDadosUnidadeCurricular) {
+                return message.SUCCESS_DELETED_ITEM //200
+            } else {
+                return message.ERROR_INTERNAL_SERVER //500
+            }
+        }
+    }
+}
 
 module.exports = {
-    inserirUnidadeCurricular
+    inserirUnidadeCurricular,
+    atualizarUnidadeCurricular,
+    getBuscarUnidadeCurricularID,
+    getUnidadesCurriculares,
+    deletarUnidadeCurricular
 }
